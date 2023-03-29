36f14785b88763fe9d20d447a0ed014c5b24c29d
marquise Lewis <danielsmarquise87@gmail.com>
2023-03-29 16:00:28 -0400

    Init, adding code for submission

---
+++ main.py
@@ -0,0 +1,34 @@
+def run_day_trip_generator():...
+destinations=("New York")
+restuarants=("Longhorn Steak House")
+entertainment=("Brunch")
+transportation=("Plane")
+
+print(destinations)
+print(restuarants)
+print(entertainment)
+print(transportation)
+
+full_trip=run_day_trip_generator()
+
+print(full_trip)
+
+def print_full_trip(list_of_otions):...
+favorite_places_to_travel=destinations[" New York" , "Atlanta", "Texas" , "Florida"]
+great_places_to_eat=restuarants["Longhorn Steak house" , "Red Lobster" , "Casa Cana"]
+things_to_do=entertainment["Brunch", "Concert" , " Movies" , "Bowling"]
+best_ways_to_travel=transportation("Plane" , "Train" , "Uber" , "Scooter")
+
+
+def generate_random_item(list_of_items):...
+list_of_items=generate_random_item["Bags", "Bowling Balls" , "Bikes" , "Golf Clubs"]
+print(list_of_items)
+
+
+def determine_satisfaction(current_trip, trip_options):...
+
+if current_trip==trip_options():
+    
+    input(current_trip)
+    print('How Satisfied are you with your trip ?')
+    
